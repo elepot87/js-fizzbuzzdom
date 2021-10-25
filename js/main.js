23cd66a469037c337155a5862d08b83ddb0752f1
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-10-25 15:35:48 +0200

    add first for loop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5 (vedi grafica allegata).
+
+// Da 1 a 100 box nel DOM
+
+// Seleziono il contenitore degli square
+const row = document.querySelector(".row");
+
+// Generare gli square
+
+for (let i = 1; i <= 100; i++) {
+  // inserisci nuovo square
+  row.innerHTML += `<div class=square>${i}</div>`;
+}
